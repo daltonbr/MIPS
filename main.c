97220ea0dfb0bd79fb5b41dfc6f700845b8ed740
daltonbr <sacinopatinete@gmail.com>
2015-05-23 19:42:38 -0300

    First Commit

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+Este eh um programa que simula um processador MIPS em C
+
+autores:
+Dalton Lima - sacinopatinete@gmail.com
+
+*/
+int main () 
+{
+	printf("Hello MIPS!\n", );
+}
